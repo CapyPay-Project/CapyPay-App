02c1a4fa9d96b4eb09275014efbd4d2d414422f2
Luis David <slimekawaii91@gmail.com>
2026-02-17 17:25:43 -0400

    refactor(ui): implement atomic design system and reorganize folder structure

---
+++ src/components/widgets/index.ts
@@ -0,0 +1,4 @@
+export { default as HistoryCard } from "./HistoryCard.astro";
+export { default as BalanceCard } from "./BalanceCard.astro";
+export { default as TransferWidget } from "./TransferWidget.astro";
+// Add other widgets as needed
